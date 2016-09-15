d8458130dfd946a0b707d9b6cb23910f7a206925
Masaya Kazama <miyan@t-p.jp>
2016-09-15 17:56:17 +0900

    no message

--- keyboards/ergodox/keymaps/miyaoka_osx/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka_osx/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [{  |           |  ]}  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | TG L1|      |      | LAlt | Cmd  |                                       | MO L1|  F7  |  F10 |      |      |
+ *   | TG L1|      | LAlt | Cmd  | MO L1|                                       | MO L1|  F7  |  F10 |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | F13  | F14  |       | F15  |  F16   |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LPRN,
     KC_LCTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LBRC,
-    TG(L1),     KC_TRNS,    KC_TRNS,    KC_LALT,    KC_LGUI,
+    TG(L1),     KC_TRNS,    KC_LALT,    KC_LGUI,    MO(L1),
 
                 KC_F13,     KC_F14,
                             KC_TRNS,
@@ -61,30 +61,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 | Home   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        | MWH_L| M_U  | MWH_R| MWH_D|      |      |           | MWH_U| PgUp | Home |  Up  | End  |      | PgUp   |
+ * |        |      | MWH_L| M_U  | MWH_R| MWH_D|      |           | MWH_U| PgUp | Home |  Up  | End  |      | PgUp   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | M_L  | M_D  | M_R  | MWH_U|      |------|           |------| PgDn | Left | Down | Right|      | PgDn   |
+ * |        |      | M_L  | M_D  | M_R  | MWH_U|------|           |------| PgDn | Left | Down | Right|      | PgDn   |
  * |--------+------+------+------+------+------|      |           | MWH_D|------+------+------+------+------+--------|
  * | MAcl1  |      |      |      |      |      |      |           |      |      |      |      |      |      | End    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      | L2   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | MBtn3|      |       |      |      |
+ *                                        | MBtn3|      |       |      | Mbtn3|
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 | MBtn1| MBtn2|------|       |------| MWH_L| MWH_R|
+ *                                 | MBtn1| MBtn2|------|       |------| Mbtn1| Mbtn2|
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// L1
+// Mouse & Cursor
 [L1] = KEYMAP(
     // left hand
-    KC_TRNS,      KC_F1,          KC_F2,        KC_F3,          KC_F4,        KC_F5,      KC_F11,
-    KC_TRNS,      KC_MS_WH_LEFT,  KC_MS_U,      KC_MS_WH_RIGHT, KC_MS_WH_DOWN,KC_TRNS,    KC_TRNS,
-    KC_TRNS,      KC_MS_L,        KC_MS_D,      KC_MS_R,        KC_MS_WH_UP,  KC_TRNS,
-    KC_MS_ACCEL1, KC_TRNS,        KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,    KC_TRNS,
-    KC_TRNS,      KC_TRNS,        KC_TRNS,      KC_TRNS,        KC_TRNS,
+    KC_TRNS,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_F11,
+    KC_TRNS,      KC_TRNS,        KC_MS_WH_LEFT,  KC_MS_U,        KC_MS_WH_RIGHT, KC_MS_WH_DOWN,    KC_TRNS,
+    KC_TRNS,      KC_TRNS,        KC_MS_L,        KC_MS_D,        KC_MS_R,        KC_MS_WH_UP,
+    KC_MS_ACCEL1, KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,          KC_TRNS,
+    KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
 
                   KC_MS_BTN3,     KC_TRNS,
                                   KC_TRNS,
@@ -97,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MS_WH_DOWN,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_END,
                                 KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    MO(L2),
 
-    KC_TRNS,    KC_TRNS,
+    KC_TRNS,    KC_MS_BTN3,
     KC_TRNS,
-    KC_TRNS,    KC_TRNS,    KC_TRNS
+    KC_TRNS,    KC_MS_BTN1,   KC_MS_BTN2
 ),
 /* Keymap 2:
  *
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// L2
+// Reset
 [L2] = KEYMAP(
     // left hand
     RESET,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
