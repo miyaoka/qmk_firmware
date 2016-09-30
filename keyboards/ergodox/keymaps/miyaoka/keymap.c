6fc957b93d945c034cbddf88c0bf58297769c3c8
Masaya Kazama <miyan@t-p.jp>
2016-09-30 12:08:16 +0900

    Cmd,Shift,Layerキーを中央化。Quotをセミコロンの横にして各種入れ替え

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -14,48 +14,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |  '"  |           |  =+  |   6  |   7  |   8  |   9  |   0  |   `~   |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  `~  |           |  =+  |   6  |   7  |   8  |   9  |   0  |   \|   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  (   |           |  )   |   Y  |   U  |   I  |   O  |   P  |   \|   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  (   |           |  )   |   Y  |   U  |   I  |   O  |   P  |   -_   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   -_   |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
  * |--------+------+------+------+------+------|  [{  |           |  ]}  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | LAlt | Cmd  |                                       | -> CR|  BS  |  F10 | DEL  |      |
+ *   |      |      |      | LAlt | Space|                                       | Ent  |  BS  |      | DEL  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |ctl F2|      |       |ctl sp| ctl F3 |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
- *                                 | Space| EISU |------|       |------| KANA   | Ent  |
- *                                 |      |      |      |       |      |        |      |
+ *                                 | Cmd  |LShift|------|       |------| KANA   | ->CR |
+ *                                 |      |      |      |       | EISU |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [L_BASE] = KEYMAP(  // layer 0 : default
     // left hand
-    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_QUOT,
+    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_GRV,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LPRN,
     KC_LCTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LBRC,
-    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LALT,    KC_LGUI,
+    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LALT,    KC_SPC,
 
-                LCTL(KC_F2),     KC_TRNS,
+                LCTL(KC_F2),KC_TRNS,
                             KC_TRNS,
-    KC_SPACE,   M(0),       KC_TRNS,
+    KC_LGUI,    KC_LSFT,    KC_TRNS,
 
     //righthand
-    KC_EQL,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,
-    KC_RPRN,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
-                KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_MINS,
+    KC_EQL,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSLS,
+    KC_RPRN,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_MINS,
+                KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_RBRC,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-                            MO(L_MOUS), KC_BSPC,    KC_F10,     KC_DEL,     KC_TRNS,
+                            KC_ENT,     KC_BSPC,    KC_TRNS,    KC_DEL,     KC_TRNS,
 
     LCTL(KC_SPC),     LCTL(KC_F3),
     KC_TRNS,
-    KC_TRNS,    M(1),       KC_ENT
+    M(0),       M(1),       MO(L_MOUS)
 ),
 
 /* Keymap: Mouse Layer
