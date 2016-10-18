a6f241d09003823e93b425465dd4f67508e44501
Masaya Kazama <miyan@t-p.jp>
2016-10-18 19:35:00 +0900

    日本語入力用に K, Y, J を優先配置

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -135,11 +135,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   /? |  ,<  |  .>  |   P  |   Y  |      |           |      |   F  |   G  |   C  |   R  |   L  |        |
+ * |        |   /? |  ,<  |  .>  |   Y  |   P  |      |           |      |   F  |   G  |   K  |   R  |   L  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   A  |   O  |   E  |   U  |   I  |------|           |------|   D  |   H  |   T  |   N  |   S  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   ;: |   Q  |   J  |   K  |   X  |      |           |      |   B  |   M  |   W  |   V  |   Z  |        |
+ * |        |   ;: |   Q  |   C  |   J  |   X  |      |           |      |   B  |   M  |   W  |   V  |   Z  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -154,9 +154,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_DVK] = KEYMAP(
     // left hand
     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-    KC_TRNS,    KC_SLSH,    KC_COMM,    KC_DOT,     KC_P,       KC_Y,       KC_TRNS,
+    KC_TRNS,    KC_SLSH,    KC_COMM,    KC_DOT,     KC_Y,       KC_P,       KC_TRNS,
     KC_TRNS,    KC_A,       KC_O,       KC_E,       KC_U,       KC_I,
-    KC_TRNS,    KC_SCLN,    KC_Q,       KC_J,       KC_K,       KC_X,       KC_TRNS,
+    KC_TRNS,    KC_SCLN,    KC_Q,       KC_C,       KC_J,       KC_X,       KC_TRNS,
     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 
                 KC_TRNS,    KC_TRNS,
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     //righthand
     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-    KC_TRNS,    KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_TRNS,
+    KC_TRNS,    KC_F,       KC_G,       KC_K,       KC_R,       KC_L,       KC_TRNS,
                 KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_TRNS,
     KC_TRNS,    KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,       KC_TRNS,
                             KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
