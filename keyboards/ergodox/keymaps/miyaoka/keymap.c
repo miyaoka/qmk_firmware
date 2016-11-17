46c28587de5741ea6ed5efe53a075c4ac0f00f7e
Masaya Kazama <miyan@t-p.jp>
2016-11-17 12:37:08 +0900

    カーソルレイヤー時にもホールド修飾キー追加

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -343,7 +343,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_F11,
     KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_M_U,         KC_TRNS,        KC_TRNS,          KC_LPRN,
     KC_TRNS,      KC_TRNS,        KC_M_L,         KC_M_D,         KC_M_R,         KC_TRNS,
-    KC_TRNS,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,             SC(KC_TAB),
+    KC_TRNS,      SFT_T(KC_1),           KC_2,           KC_3,           KC_4,           KC_5,             SC(KC_TAB),
     KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_M_B3,        KC_M_B2,
 
                   KC_TRNS,        KC_TRNS,
@@ -354,7 +354,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F12,       KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,           MO(L_CONF),
     KC_RPRN,      KC_HOME,        KC_PGUP,        KC_UP,          KC_PGDN,        KC_TRNS,          KC_TRNS,
                   KC_END,         KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_COLN,          KC_TRNS,
-    C(KC_TAB),    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,             KC_TRNS,
+    C(KC_TAB),    KC_6,           KC_7,           KC_8,           KC_9,           SFT_T(KC_0),             KC_TRNS,
                                   KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,          KC_TRNS,
 
     KC_TRNS,      KC_TRNS,
