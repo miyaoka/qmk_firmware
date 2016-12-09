ab393562584899131ba70c396cedc834478e2b3e
Masaya Kazama <miyan@t-p.jp>
2016-12-09 15:32:54 +0900

    Drop LED action

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -404,25 +404,6 @@ void matrix_init_user(void) {
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 
-  uint8_t layer = biton32(layer_state);
-
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    // TODO: Make this relevant to the ErgoDox EZ.
-    case 1:
-      ergodox_right_led_1_on();
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      break;
-    default:
-      // none
-      break;
-  }
-
 };
 void dance_lang_finished (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
