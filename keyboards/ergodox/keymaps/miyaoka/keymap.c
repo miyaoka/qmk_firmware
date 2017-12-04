7baaef074cb8cb5ff83ebaa843f68f56701ea747
Masaya Kazama <miyan@t-p.jp>
2017-12-04 11:41:08 +0900

    Add layer key to left-ESC

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
               _______,    _______,
                           _______,
-  _______,    CK_A_EN,    KC_ESC,
+  _______,    CK_A_EN,    LT(L_NUM, KC_ESC),
 
 
   //righthand
