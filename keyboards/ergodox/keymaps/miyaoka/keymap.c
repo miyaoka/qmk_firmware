b3b4fdca4c316906ff07ca9009a3eda214415963
Masaya Kazama <miyan@t-p.jp>
2017-12-04 14:58:05 +0900

    Move Home/End keys to lefthand

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -441,9 +441,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 | ->CNF  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      | PREV | M_U  | NEXT | MW_U |  <   |           |  >   | Home | PgDn | PgUp |  End |      |        |
+ * |        |      | PREV | M_U  | NEXT | Home |  <   |           |  >   |      | PgDn | PgUp |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |   (    |   ?  | M_L  | M_D  | M_R  | MW_D |------|           |------| Left | Down |  Up  | Right|   :  |   )    |
+ * |   (    |   ?  | M_L  | M_D  | M_R  | End  |------|           |------| Left | Down |  Up  | Right|   :  |   )    |
  * |--------+------+------+------+------+------|      |           |  /   |------+------+------+------+------+--------|
  * |        |   !  |   @  |   #  |   $  |   %  |      |           |      |   ^  |   &  |   *  |   (  |   )  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -460,8 +460,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [L_NAV2] = KEYMAP(
   // left hand
   _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F11,
-  _______,    _______,    KC_PREV,    KC_M_U,     KC_NEXT,    KC_M_WU,    KC_LT,
-  KC_LPRN,    KC_QUES,    KC_M_L,     KC_M_D,     KC_M_R,     KC_M_WD,
+  _______,    _______,    KC_PREV,    KC_M_U,     KC_NEXT,    KC_HOME,    KC_LT,
+  KC_LPRN,    KC_QUES,    KC_M_L,     KC_M_D,     KC_M_R,     KC_END,
   _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    _______,
   _______,    KC_LGUI,    _______,    _______,    KC_M_B3,
 
@@ -471,7 +471,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //righthand
   KC_F12,     KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     MO(L_CONF),
-  KC_GT,      KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,
+  KC_GT,      _______,    KC_PGDN,    KC_PGUP,    _______,     _______,    _______,
               KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   KC_COLN,    KC_RPRN,
   KC_SLSH,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_M_A0,
                           KC_PIPE,    _______,    _______,    _______,    _______,
