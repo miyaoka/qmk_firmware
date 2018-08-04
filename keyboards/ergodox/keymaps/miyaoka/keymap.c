2d72f0d32dccffb75069fabe02ea80a5814384d3
Masaya Kazama <miyan@t-p.jp>
2018-08-04 14:29:30 +0900

    Add alt key on nav layer

--- keyboards/ergodox/keymaps/miyaoka/keymap.c
+++ keyboards/ergodox/keymaps/miyaoka/keymap.c
@@ -441,7 +441,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 | ->CNF  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      | PREV | M_U  | NEXT | Home |      |           | MW_D |      | PgDn | PgUp | Space|      |        |
+ * |        |      | PREV | M_U  | NEXT | Home |      |           | MW_D |      | PgDn | PgUp | Space| LAlt |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |   (    |   ?  | M_L  | M_D  | M_R  | End  |------|           |------| Left | Down |  Up  | Right|   :  |   )    |
  * |--------+------+------+------+------+------|      |           | MW_U |------+------+------+------+------+--------|
@@ -471,7 +471,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //righthand
   KC_F12,     KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     MO(L_CONF),
-  KC_M_WD,    _______,    KC_PGDN,    KC_PGUP,    KC_SPC,     _______,    _______,
+  KC_M_WD,    _______,    KC_PGDN,    KC_PGUP,    KC_SPC,     KC_LALT,    _______,
               KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   KC_COLN,    KC_RPRN,
   KC_M_WU,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_M_A0,
                           KC_PIPE,    _______,    _______,    _______,    _______,
